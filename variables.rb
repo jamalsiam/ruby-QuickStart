96cfe019ae42276aaba054c59b5d28bed2c27d74
jamalsiam <jamalsiam1995@gmail.com>
2017-10-09 18:55:30 +0300

    variables

---
+++ variables.rb
@@ -0,0 +1,20 @@
+x=22;
+puts x;
+puts x.object_id;
+puts x.class;
+
+y=10.5;
+puts y.to_i;
+puts y;
+puts y.object_id;
+puts y.class;
+
+
+z="str";
+puts z;
+puts z.object_id;
+puts z.class;
+
+
+
+
