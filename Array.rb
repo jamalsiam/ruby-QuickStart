d1215e91389fa610dc709bf78953bc3b68514e64
jamalsiam <jamalsiam1995@gmail.com>
2017-10-09 21:16:00 +0300

    array

---
+++ Array.rb
@@ -0,0 +1,39 @@
+arr=[23,76,54,78,55,55,1,1,44]
+
+#access
+puts arr[0];#23
+puts arr[5];#1
+puts arr[100]; #nil or space;
+
+
+#change value from array
+arr[0]=4;
+arr[1]=8;
+arr[2]="hi";
+arr[3]=45;
+puts arr;
+
+#add in array
+arr<<14;
+arr.push(34);
+puts "*"
+puts arr
+puts "*"
+
+#delete from array
+arr.delete_at(2); #delete by endex
+arr.delete(1) # delete by value
+arr.pop #delete last index
+puts arr
+
+#some method
+puts "*"
+puts arr.sort
+puts "*"
+puts arr.uniq
+
+#method spilt
+str="my name is jamal"
+puts str.split("m") # puts=>["","y na","e","e is ja","al"]:=spilt dived string to array where pattern is defined
+
+
